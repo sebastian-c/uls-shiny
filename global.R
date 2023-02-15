66a897e2234d5ad80436e95290df528ad64044df
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2023-02-15 15:44:09 +0100

    Removed last trace of iris dependency

--- global.R
+++ global.R
@@ -10,11 +10,6 @@ library(ggplot2)
 library(dplyr)
 library(DT)
 
-list_quantitative_columns <- c("Sepal.Length",
-                               "Sepal.Width",
-                               "Petal.Length",
-                               "Petal.Width")
-
 getConditionColNames <- function(data, condition){
   names(data)[vapply(data, condition, logical(1))]
 }
